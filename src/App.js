6e056f3d182f490941ef8381709283e4b18d3cf4
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2024-07-11 13:18:07 -0500

    feat: Going over React learning tutorial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+    name: 'Bobby Bit Byt',
+    iamgeUrl: 'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350',
+    imageSize: 90,
+}
+
+function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
+    );
+}
+
+function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+            <AboutPage />
+        </>
+    );
+}
+
+function MyButton() {
+    return (
+        <button>
+            I'am a button
+        </button>
+    );
+}
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
+}
+
 function App() {
   return (
     <div className="App">
@@ -22,4 +72,6 @@ function App() {
   );
 }
 
-export default App;
+{/*export default App;*/}
+{/*export default MyApp;*/}
+export default Profile;
