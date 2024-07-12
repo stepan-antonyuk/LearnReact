5e4a873b0707804c203c35e7bf11cefeb2eeefca
Stepan Antonyuk <stepan.antonyuk@gmail.com>
2024-07-12 17:15:02 -0500

    feat: finished firsh React tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const user = {
     name: 'Bobby Bit Byt',
@@ -72,6 +73,99 @@ function App() {
   );
 }
 
-{/*export default App;*/}
-{/*export default MyApp;*/}
-export default Profile;
+// just if else test
+//function IfElse() {
+//    isLoggedIn = false;
+//    let content;
+//    if (isLoggedIn) {
+//        content = <AdminPanel />;
+//    } else {
+//        content = <LoginForm />;
+//    }
+//    return (
+//        <div>
+//            {content}
+//        </div>
+//    );
+//}
+
+const products = [
+    {title: 'Cabbage',isFruit: false, id: 1},
+    {title: 'Garlic',isFruit: false, id: 2},
+    {title: 'Apple',isFruit: true, id : 3},
+]
+
+function ShoppingList() {
+    const listItems = products.map(product =>
+        <li key={products.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+function ClickButton() {
+    const [count, setCount] = useState(0);
+
+    function OnClick() {
+        //alert('You clicked me!');
+        setCount(count + 1);
+    }
+
+    return (
+        <button onClick={OnClick}>
+            You Clicked me {count} times!
+        </button>
+    );
+}
+
+function TwoClickButtons() {
+    return (
+        <div>
+            <h1>Two Buttons</h1>
+            <ClickButton />
+            <br />
+            <ClickButton />
+        </div>
+    );
+}
+
+function ShareClickButton({count, onClick}) {
+    return (
+        <button onClick={onClick}>
+            You Clicked me {count} times!
+        </button>
+    );
+}
+
+function TwoClickButtonsShare() {
+    const [count, setCount] = useState(0);
+
+    function OnClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>Two Buttons sharring counter</h1>
+            <ShareClickButton count={count} onClick={OnClick} />
+            <br />
+            <ShareClickButton count={count} onClick={OnClick} />
+        </div>
+    );
+}
+
+//export default App;
+//export default MyApp;
+//export default Profile;
+//export default ShoppingList;
+//export default ClickButton;
+//export default TwoClickButtons;
+export default TwoClickButtonsShare;
